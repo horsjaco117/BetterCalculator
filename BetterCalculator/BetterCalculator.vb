3e07c2e9652d4a3d30e2e03551f7cb5f469c43db
horsjaco117 <jacob_horsley@live.com>
2025-02-02 20:04:28 -0700

    Bugs fixed and code it working all basic functions.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -1,36 +1,45 @@
-﻿Module Module1
+﻿'Jacob Horsley 
+'Spring 2025
+'RCET 2265
+'Better Calculator
+' 
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module BetterCalculator
 
     Sub Main()
         Dim firstNumber As Integer
         Dim secondNumber As Integer
         Dim getOutofHere As Boolean = False
-        Dim userinput As String
-
-        Console.WriteLine("Please enter two numbers. Enter 'Q' at any time to quite")
-        Console.WriteLine()
+        Dim userInput As String
 
         Do
             'Get first number from user
             'Always runs at least once
             'run until valid number or user quits
             Do
-                Console.WriteLine("Choose a Number")
-                userinput = Console.ReadLine()
-                Console.WriteLine($"you entered {userinput}")
+                Console.WriteLine("Please enter two numbers. Enter 'Q' at any time to quit.")
+                userInput = Console.ReadLine()
+                Console.WriteLine($"you entered {userInput}")
 
                 Try
-                    firstNumber = CInt(userinput)
+                    firstNumber = CInt(userInput)
                     getOutofHere = True
 
                 Catch ex As Exception
-                    If userinput <> "Q" Then
-                        Console.WriteLine($"You entered {userinput},
-                            please enter a whole number")
+                    If userInput <> "Q" Then
+
+                        Console.WriteLine("Please enter a whole number")
 
                     Else
                         getOutofHere = True
+                        Console.WriteLine("Have a nice day")
                     End If
                 End Try
+
 
             Loop Until getOutofHere = True
 
@@ -38,51 +47,55 @@
             'only runs if user has not already quit
             'run until valid number or user quits
             getOutofHere = False 'reset flag
-            Do While userinput <> "Q" And getOutofHere = False
+            Do While userInput <> "Q" And getOutofHere = False
                 Console.WriteLine("Choose a Number")
-                userinput = Console.ReadLine()
-                Console.WriteLine($"you entered {userinput}")
+                userInput = Console.ReadLine()
+                Console.WriteLine($"you entered {userInput}")
 
                 Try
-                    secondNumber = CInt(userinput)
+                    secondNumber = CInt(userInput)
                     getOutofHere = True
 
                 Catch ex As Exception
-                    If userinput <> "Q" Then
-                        Console.WriteLine($"Userinput is not a whole number")
+                    If userInput <> "Q" Then
+                        Console.WriteLine($"{userInput} is not a whole number")
                     Else
                         getOutofHere = True
+                        Console.WriteLine("Have a nice day.")
+                        Exit Sub
                     End If
                 End Try
-
             Loop
+
 
             'get user choice only runs if user has not already quit
             'run until valid choice or user quits
 
             Console.WriteLine("Choose one of the following options" & vbNewLine _
-                      & "1. add" & vbNewLine _
-                      & "2. Subtract" & vbNewLine _
-                      & "3. Multiply" & vbNewLine _
-                      & "4. Divide")
+              & "1. add" & vbNewLine _
+              & "2. Subtract" & vbNewLine _
+              & "3. Multiply" & vbNewLine _
+              & "4. Divide")
 
-            userinput = Console.ReadLine() ' user decides
+            userInput = Console.ReadLine() ' user decides
 
             'Results of userInput
 
-            If userinput = "1" Then
+            If userInput = "1" Then
                 Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
-            ElseIf userinput = "2" Then
+            ElseIf userInput = "2" Then
                 Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
-            ElseIf userinput = "3" Then
+            ElseIf userInput = "3" Then
                 Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}")
-            ElseIf userinput = "4" Then
+            ElseIf userInput = "4" Then
                 Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}")
+            ElseIf userInput = "Q" Then
+                Console.WriteLine("Have a nice day")
             Else
-                Console.WriteLine($"You entered {userinput}")
+                Console.WriteLine($"You entered {userInput}")
             End If
 
-        Loop While userinput <> "Q"
+        Loop While userInput <> "Q"
 
 
     End Sub
